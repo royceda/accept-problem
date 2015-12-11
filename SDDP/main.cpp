ad35dbea5c1eb2fd4cf5b046b351218f010e4490
royceda <caucricity@gmail.com>
2015-12-11 11:24:27 +0100

    first

---
+++ SDDP/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: rboudjeltia
+ *
+ * Created on 11 décembre 2015, 10:29
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include "ilcplex/ilocplex.h"
+
+ILOSTLBEGIN
+        
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    IloEnv env;
+    
+    cout << "YOUPII!!" << endl;
+    
+    
+    return 0;
+}
+
