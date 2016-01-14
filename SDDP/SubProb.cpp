834d0f573abfc1329f75aad9dce9a6febeccc96e
royceda <caucricity@gmail.com>
2016-01-14 01:18:08 +0100

    cleaner

--- SDDP/SubProb.cpp
+++ SDDP/SubProb.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   SubProb.cpp
  * Author: rboudjeltia
- * 
+ *
  * Created on 18 décembre 2015, 11:00
  */
 
@@ -9,147 +9,168 @@
 
 SubProb::SubProb() {}
 
-SubProb::SubProb(IloEnv &env): _env(env) {}
+SubProb::SubProb(IloEnv &env1): env(env1) {}
 
 SubProb::SubProb(const SubProb& orig) {}
 
-SubProb::~SubProb() {}
+SubProb::~SubProb() {
+  constraints.end();
+  model.end();
+}
+
+IloNumArray SubProb::getE(){
+  return _E;
+}
+
+IloNum SubProb::gete(){
+  return _e;
+}
 
 
+SubProb::SubProb(IloEnv &env1, IloNumArray x, IloNum theta, Parser &p):env(env1), _x(x),
+_theta(theta), D(p.capacity()), n(p.nbCommands()), S(p.nbScenario()), y(env, S), _e(0),
+primal(env, S), pi(env, S), constraints(env), d(p.durationScenarTask()),
+proba(p.probaVector()), sub(p.subTreatedCost()){
+  /*initilization for each scenario*/
+  for (unsigned int i = 0; i < S; i++) {
+    y[i]      = IloNumVarArray(env, S, 0, 1, ILOBOOL);
+    pi[i]     = IloNumArray(env, 2*n); //car 2n contrainte
+    primal[i] = IloNumArray(env, n);
+    model[i]  = IloModel(env);
+  }
+}
 
-/**
- * 
- * @return Generate the new cut from the sub problem
- */
-IloExpr SubProb::solve(Parser &p){
+bool SubProb::K2Test(){
 
-  IloEnv env;
-  
+  return true;
+}
+
+
+bool SubProb::optimTest(){
+  IloNum w;
+  IloNum a;
+
+  for(unsigned int i=0; i<n; i++){
+    a += _E[i] * _x[i];
+  }
+
+  w = a - _e;
+
+  if( _theta >= w){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+void SubProb::initScenario(){
   try{
-    vector<IloModel> model;
-  
-    int S = p.nbScenario();
-    int n = p.nbCommands();
-    int D = p.capacity();
-    
-    
-    IloArray <IloNumVarArray> y(env, S); //other commands
-    IloNumArray  x(env);//Primal solution of master problem (given)
-    IloArray <IloNumArray> primal(env, S); //primal values of subprob
-    IloArray <IloNumArray> pi(env, S); //dual values of subprob
-    
-     IloArray <IloRangeArray>  constraints(env); //Constraints
-    
-    
-    /*initilization*/
-    for (int i = 0; i < S; i++) {
-      y[i] = IloNumVarArray(env, S, 0, 1, ILOBOOL);
-    }
-
-    
-    for( int i = 0; i < S; i++){
-      pi[i] = IloNumArray(env, n);
-      primal[i] = IloNumArray(env, n);
-        
-    }
-     
-    
-     
-  
-     
-     
-     
-     
-     
-     
-     
-    /*Definition of s subProblems for each scenario*/
-    for( int s = 0; s < S; s++){
-
+    /*step 3*/
+    /*Definition of S subProblems for each scenario*/
+    #pragma omp parrallel for
+    for(unsigned int s = 0; s < S; s++){
       /*Objective*/
       IloExpr e0(env);
-      for(int i = 0; i<n; i++){
-          e0 += y[i][s] * p.subTreatedCost()[i]; ; 
+      for(unsigned int i = 0; i<n; i++){
+        e0 += y[i][s] * sub[i]; ;
       }
       IloObjective obj(env, e0, IloObjective::Maximize, "OBJ");
       model[s].add(obj);
-      
-      
-    
-      
-      //for (unsigned int k = 0; k < s; k++) {
+
+
       IloExpr e1(env);
-      for (unsigned int i = 0; i < n; i++) {
-          e1 += p.durationScenarTask()[i][s] * (x[i] - y[i][s]); //d is vector of demands
+
+      for (unsigned int j = 0; j < n; j++) { // first part: n constraints
+        for (unsigned int i = 0; i < n; i++) { // matrix prod
+          e1 += d[i][s] * (_x[i] - y[i][s]); //d is vector of demands
+        }
+        constraints[s].add(e1 <= D); // D is the total duration
+        e1.end();
       }
-      constraints[s].add(e1 <= D); // D is the total duration
-      e1.end();
-      //}
-      
-      
-      for (unsigned int i = 0; i < n; i++) {
-          //for (unsigned int j = 0; j < s; s++) {
-          IloExpr tmp(env);
-          tmp = y[i][s];
-          
-          constraints[s].add(y[i][s]<= x[i]); // [order][scenar]
-          tmp.end();
-          //}
+
+      #pragma omp parrallel for
+      for (unsigned int i = 0; i < n; i++) { //second part: n constraints
+        IloExpr tmp(env);
+        tmp = y[i][s];
+
+        constraints[s].add(y[i][s] <= _x[i]); // [order][scenar]
+        tmp.end();
       }
-      model[s].add(constraints[s] );
+      model[s].add(constraints[s]);
     }
-    
-     
-     
-    
-    /* resolve subProb and dual*/
-    /**
-     * TODO 
-     * get primal solution y
-     * get dual solution pi
-     */
-    for( int s = 0; s < S; s++){
-        //getvalues sol
+
+
+    for(unsigned int s = 0; s < S; s++){
       IloCplex cplex(model[s]);
       cplex.getValues(primal[s], y[s]);
       cplex.getDuals(pi[s], constraints[s]);
-        
     }
-     constraints.end();
-    
-    
-    
-    /*creation of E and e from pi*/
-    IloExpr e(env); 
-    for( int i = 0; i < S; i++){
-      for(int k = 0; k < n; k++ )
-	e += p.probaVector()[i] * pi[i][k] * D;         
-    }
-     
-     
-    IloExpr E(env);
-    for( int i = 0; i < S; i++){
-      for(int k = 0; k < n; k++ ){
-	E += p.probaVector()[i] * pi[i][k] * p.durationScenarTask()[i][k];                
-      }
-    }
-     
-    /*check theta validity*/
-    /*check theta validity*/
-    /*stop if theta > w*/
-     
-     
-    /*cut creation*/
-    IloExpr cut(env);
-
-    for(int k = 0; k < n; k++ ){
-      cut = e - E*x[k];
-    }
-    return cut; // just to trace the generation
   }catch (IloException& e) {
     cerr << "ERROR : " << e << "\n";
   }
-  env.end();
-  return NULL; //return an iloRangeArray which is the cut
 }
 
+
+
+IloExpr SubProb::optimalCut(IloArray<IloNumVar> x){
+
+
+
+  /*creation of E and e from pi*/
+  for(unsigned int k = 0; k < S; k++){
+    IloNum pid(0);
+    for(unsigned int i = 0; i < n; i++){ //vector prod
+      pid += pi[k][i] * D;
+    }
+    _e += proba[k] * pid ;
+
+  }
+
+
+  //il y a 2n var dual car 2n contrainte
+  for(unsigned int i = 0; i < n; i++){
+    IloNumArray pit(0);
+    for(unsigned int j = 0; j < n; j++){//matrix prod part 1
+      pit[i] +=  pi[i][j] * d[j][i];
+    }
+
+    for(unsigned int j = n; j < 2*n; j++){//matrix prod part2
+      pit[i] +=  pi[i][j] * 1; //row x col2
+    }
+
+    for(unsigned int k = 0; k < S; k++){//sum with k n part 1
+      _E[i] += proba[k] * pit[k] ;
+    }
+
+
+  }
+
+  /*cut creation*/
+  IloExpr cut(env);
+
+  for(unsigned int k = 0; k < n; k++ ){
+    cut = _e - _E[k]*x[k];
+  }
+
+  return cut;
+}
+
+
+/**
+ *
+ * @return Generate the new cut from the sub problem
+ */
+IloExpr SubProb::solve(Parser &p, IloArray<IloNumVar> x){
+  IloExpr cut(env);
+  if(K2Test()){
+    cut = feasibleCut(x);
+    return cut;
+  }
+  else{
+    initScenario();
+    cut = optimalCut(x);
+    return cut;
+  }
+  return NULL;
+}
--- SDDP/SubProb.h
+++ SDDP/SubProb.h
@@ -1,10 +1,16 @@
-/* 
+/*
  * File:   SubProb.h
  * Author: rboudjeltia
  *
  * Created on 18 décembre 2015, 10:59
  */
 
+
+/**
+*TODO
+* feasibible cut
+* K2 test
+*/
 #ifndef SUBPROB_H
 #define	SUBPROB_H
 
@@ -13,20 +19,52 @@
 #include "Parser.h"
 
 class SubProb {
+
 public:
-    SubProb();
-    SubProb(IloEnv &env);
-    SubProb(const SubProb& orig);
-    virtual ~SubProb();
-    
-    // IloEnv env();
-    
-    IloExpr solve(Parser &p); 
-    
-    
+  SubProb();
+  SubProb(IloEnv &env1);
+  SubProb(IloEnv &env1, IloNumArray x, IloNum theta, Parser &p); //done
+  SubProb(const SubProb& orig);
+  virtual ~SubProb();
+
+  IloExpr solve(Parser &p, IloArray<IloNumVar> x);
+  IloExpr optimalCut(IloArray<IloNumVar> x); //TODO
+  IloExpr feasibleCut(IloArray<IloNumVar> x); //TODO
+
+  IloNumArray getE();
+  IloNum gete();
+
+
+protected:
+  bool K2Test(); //TODO
+
+  void initScenario();
+  bool optimTest();
+
+
 private:
-    IloEnv _env;
+  IloEnv env;
+  IloNum _theta;
+  IloNumArray _x;
+
+  //vector<IloObjective> obj;
+  vector<IloModel> model;
+  IloArray <IloRangeArray> constraints;
+  IloArray <IloNumArray>  primal; //primal values of subprob
+  IloArray <IloNumArray> pi;  //dual values of subprob
+  IloArray <IloNumVarArray> y; //other commands
+
+  IloNumArray _E;
+  IloNum _e;
+
+
+  int S; // nb scenario
+  int n; // nb Comands
+  int D; // Capacity
+  vector<int> sub; //subTreatedCost
+  vector<vector<int> > d; //demands a verif
+  vector<double> proba;
+
 };
 
 #endif	/* SUBPROB_H */
-
